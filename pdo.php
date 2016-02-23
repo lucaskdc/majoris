c8f52b4dadfec305dbcd594ebd33a8dcb18b3c57
lucaskdc <lucas.dalcastel@hotmail.com>
2016-02-23 16:09:07 -0300

    utf8 pdo connection

--- pdo.php
+++ pdo.php
@@ -5,6 +5,7 @@ function getConnection (){
         $pdo = new PDO('mysql:host='.$host.';dbname='. $dbname , $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+        $pdo->exec('SET NAMES utf8');
     } catch (PDOException $e){
         echo $e->getMessage();
         die($e);
